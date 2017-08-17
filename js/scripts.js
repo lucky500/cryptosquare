6ceb22f9c886f9eba82d3c77654f4d7b5b18660d
Kristen Kulha and Luciano Oliveira <luciano@>
2017-08-17 15:19:42 -0700

    Add function to find closest square to length of array of characters in sentence.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 //Business Logic
 var sentenceArray; // remember to move into function later.
 var inputValue;
+var rows;
+var columns;
 function cryptoSquare(input){
   if(!isNaN(input)){
     alert("Please enter a sentence!");
@@ -8,8 +10,17 @@ function cryptoSquare(input){
     inputValue = inputValue.toLowerCase();
     inputValue = inputValue.replace(/[\s\W]/g, "");
     sentenceArray = inputValue.split("");
+    var square = findClosestSquare(sentenceArray);
     return sentenceArray;
   }
+}
+
+function findClosestSquare(array){
+   var arrayLength = array.length;
+   rows = Math.ceil(Math.sqrt(arrayLength));
+   columns =  Math.ceil(Math.sqrt(arrayLength));
+   console.log('rows:', rows);
+   console.log('columns:', columns);
 }
 
 //interface Logic
